c03f0a8310ada13bb47cc01f92c3b280ed276b87
Kostiantin Ustymenko <kmustymenko@gmail.com>
2023-03-29 22:40:41 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ghPages({
+      // Опции плагина
+      branch: "gh-pages",
+      repo: "https://github.com/KUstimenko/3d-portfolio.git",
+      dir: "dist",
+    }),
+  ],
+});
